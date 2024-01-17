818ae567d6f736b07249e596febecde78d4796cb
minkyong <76981381+mink7878@users.noreply.github.com>
2024-01-17 20:33:00 +0900

    20240117

---
+++ day23.py
@@ -0,0 +1,10 @@
+import collections
+
+def solution(v):
+    answer = []
+    
+    for i in zip(*v):
+        val = collections.Counter(i)
+        answer.append(val.most_common()[-1][0])
+        
+    return answer
