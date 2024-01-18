f6838190d1e44391c13314d7b5dbd57f930a382c
minkyong <76981381+mink7878@users.noreply.github.com>
2024-01-18 23:20:26 +0900

    20240118

--- day23.py
+++ day23.py
@@ -1,10 +1,17 @@
-import collections
+import sys
 
-def solution(v):
-    answer = []
-    
-    for i in zip(*v):
-        val = collections.Counter(i)
-        answer.append(val.most_common()[-1][0])
-        
-    return answer
+input = sys.stdin.readline
+
+N, K = map(int, input().split())  # 동전 종류 # 가치 합
+A = []
+answer = 0
+
+for _ in range(N):
+  A.append(int(input()))
+
+for i in range(N - 1, -1, -1):  # 역순 >>> sort, 갱신 x
+  if A[i] <= K:
+    answer += int(K / A[i])
+    K %= A[i]
+
+print(answer)
